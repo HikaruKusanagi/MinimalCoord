bcec0dd8e6c50e432b516025dd547b535ed38786
草薙光 <>
2022-04-05 20:38:13 +0900

    ドロップボタンの実装

--- lib/mypage/mypage_model.dart
+++ lib/mypage/mypage_model.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 class MyPageModel extends ChangeNotifier {
 
   String? value;
+  String? isSelectedItem = '150';
+  String? isSelectedItem2 = '男性';
 
-  void isSelectedItem() {
-    this.value = value;
+  void isSelectedItem3(value) {
+    isSelectedItem = value;
+    notifyListeners();
+  }
+
+  void isSelectedItem4(value) {
+    isSelectedItem2 = value;
     notifyListeners();
   }
 }
+
+
